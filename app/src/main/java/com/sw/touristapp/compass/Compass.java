7638594eb4ac05de489bbe7c3147eb579831c837
Dominik Cegiełka <224478@edu.p.lodz.pl>
2020-05-20 19:58:04 +0200

    Initial commit

---
+++ app/src/main/java/com/sw/touristapp/compass/Compass.java
@@ -0,0 +1,17 @@
+package com.sw.touristapp.compass;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+
+public class Compass implements SensorEventListener {
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+}
