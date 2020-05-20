083732554ddf6b3cd4dc2d60d7ee35593e53996a
kamilZ01 <224546@edu.p.lodz.pl>
2020-05-20 20:58:47 +0200

    initial commit 2

--- app/src/main/java/com/sw/touristapp/compass/Compass.java
+++ app/src/main/java/com/sw/touristapp/compass/Compass.java
@@ -1,10 +1,24 @@
 package com.sw.touristapp.compass;
 
+import android.content.Context;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 
 public class Compass implements SensorEventListener {
+    private SensorManager sensorManager;
+    private Sensor accelerometrSensor;
+    private Sensor geoMagneticSensor;
+
+
+
+    public Compass(Context context) {
+        sensorManager= (SensorManager) context.getSystemService(Context.SENSOR_SERVICE);
+        accelerometrSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        geoMagneticSensor = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+    }
+
     @Override
     public void onSensorChanged(SensorEvent event) {
 
